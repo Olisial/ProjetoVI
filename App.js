bdb363e3bed07c0d737869d94a04dd1e86781392
Laís <laisdeoliveira1215@gmail.com>
2025-09-18 19:56:34 -0300

    teste2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>***PROJETO VI***</Text>
+      <Text>***Proj***</Text>
       <StatusBar style="auto" />
     </View>
   );
